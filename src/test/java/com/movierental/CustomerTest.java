b75319392dcd57042868ce52f7d71c711865ed51
Pallav Dubey <pallavdubey@IN-PallavDubey-2.local>
2019-11-08 12:33:41 +0530

    adds characterization test

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -1,9 +1,27 @@
 package com.movierental;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void test(){
+        Customer customer = new Customer("Pallav");
+
+        Movie movieOne = new Movie("Midway", Movie.NEW_RELEASE);
+        Rental rentalOne = new Rental(movieOne, 1);
+
+        Movie movieTwo = new Movie("The GoodFellas", Movie.REGULAR);
+        Rental rentalTwo = new Rental(movieOne, 3);
+
+        customer.addRental(rentalOne);
+        customer.addRental(rentalTwo);
+
+        String actual = customer.statement();
+
+        Assert.assertEquals(
+            String.format("Rental Record for Pallav\n\tMidway\t3.0\n\tMidway\t9.0\nAmount owed is 12.0\nYou earned 3 frequent renter points"),
+            actual);
+    }
 
 }
